4c03f7bb006a50f5aa0b713673c22d34b45b9aa9
Turgut Taşkesen <taskesen@msn.com>
2021-11-15 16:18:02 +0300

    15 Kasım2021

---
+++ src/components/userComponent.js
@@ -0,0 +1,19 @@
+import { BaseLogger, ElasticLogger, MongoLogger } from "../crossCuttingCocerns/logging/logger.js"
+import User from "../models/user.js"
+import UserService from "../services/userService.js"
+
+console.log("User Component Yüklendi...")
+
+let logger1 = new MongoLogger()
+let userService = new UserService(logger1)
+
+let user1 = new User(1, "Turgut","Taşkesen", "Kahramanmaraş")
+let user2 = new User(2, "İbrahim","Taşkesen", "Kahramanmaraş")
+
+userService.add(user1)
+userService.add(user2)
+
+console.log(userService.list())
+console.log(userService.getById(2))
+
+userService.list()
---
+++ src/services/userService.js
@@ -0,0 +1,24 @@
+export default class UserService{
+    constructor(loggerService){
+        this.users = []
+        this.loggerService = loggerService
+    }
+
+    add(user){
+        //console.log("Kullanıcı Eklendi : " + user)
+        this.users.push(user)
+        this.loggerService.log(user)
+    }
+
+    list(){
+        //console.log("Kullanıcılar Listelendi")
+        return this.users
+    }
+
+    getById(id){
+        //console.log("Kullanıcı Detayı Getirildi.")
+        return this.users.find(u => u.id === id)
+
+    }
+
+}
